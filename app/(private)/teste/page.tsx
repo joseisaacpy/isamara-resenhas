81daa15228d3299d0eeb0e3d92441d3b7c1c254b
José Isaac <joseisaacnascimento@gmail.com>
2025-12-24 19:02:09 -0300

    feat: criar páginas vazias para testes

---
+++ app/(private)/teste/page.tsx
@@ -0,0 +1,3 @@
+export default function Teste() {
+  return <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quod possimus doloremque itaque, ipsa tempore obcaecati earum laborum minus, modi harum voluptas consequatur quas eius quam delectus tempora, quo dolores?</h1>;
+}
