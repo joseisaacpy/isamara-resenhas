31ce7c980967dedad669f45187e7207c0fe23676
José Isaac <joseisaacnascimento@gmail.com>
2025-12-24 18:47:48 -0300

    feat(proxy): criar middleware para verificar se há session

---
+++ proxy.ts
@@ -0,0 +1,32 @@
+import { NextResponse } from "next/server";
+import { prisma } from "@/lib/prisma";
+import type { NextRequest } from "next/server";
+
+export async function proxy(request: NextRequest) {
+  // pega o id da sessão
+  const sessionId = request.cookies.get("sessionId")?.value;
+
+  // tentando acessar área privada sem cookie
+  if (!sessionId) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  //   busca a session no banco
+  const session = await prisma.session.findUnique({
+    where: {
+      id: sessionId,
+    },
+  });
+
+  // se não existir session ou a session estiver expirada
+  if (!session || session.expiresAt < new Date()) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  // se existir session, passa
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/(private)/:path*"],
+};
