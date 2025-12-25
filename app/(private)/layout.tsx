cf1409c579af7c1260d3d80cccb5a2f502f5df07
José Isaac <joseisaacnascimento@gmail.com>
2025-12-25 15:18:00 -0300

    fix(layout): corrigir elementos do layout privado

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,24 +1,7 @@
-"use client";
-import { useLogout } from "@/lib/useLogout";
-import { Button } from "@/components/ui/button";
-import { LogOut } from "lucide-react";
-
 export default function PrivateLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  const { logout } = useLogout();
-  return (
-    <main>
-      <Button
-        onClick={() => {
-          logout();
-        }}
-      >
-        <LogOut className="h-4 w-4" />
-      </Button>
-      {children}
-    </main>
-  );
+  return <main>{children}</main>;
 }
