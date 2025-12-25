762f7693e3cf0a738bb2052dd8ae4523f2ddde03
José Isaac <joseisaacnascimento@gmail.com>
2025-12-25 10:00:51 -0300

    feat(not-found): criar página de erro/não encontrada

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <section>
+      <h1>Ops, página não encontrada!</h1>;
+      <Link href="/">Voltar para a página inicial</Link>
+    </section>
+  );
+}
