2e48054e062d58c3068a9944efeea33c0192007c
José Isaac <joseisaacnascimento@gmail.com>
2025-12-25 15:59:14 -0300

    feat(blog): listar as resenhas na página inicial/blog

--- app/(public)/(blog)/page.tsx
+++ app/(public)/(blog)/page.tsx
@@ -1,7 +1,24 @@
-export default function Blog() {
+import { prisma } from "@/lib/prisma";
+import type { Review } from "@prisma/client";
+
+export default async function Blog() {
+  const reviews: Review[] = await prisma.review.findMany();
   return (
     <section>
-      <h1>Blog</h1>
+      <h1>Seja bem-vindo ao meu Blog de resenhas</h1>
+      <section>
+        {/* se nenhuma review for cadastrada */}
+        {reviews.length === 0 && <h2>Não há resenhas cadastradas</h2>}
+        {/* se houver resenhas cadastradas */}
+        {reviews.map((review) => (
+          <div key={review.id} className="border p-4 rounded">
+            <h2 className="font-semibold">{review.title}</h2>
+            <p className="text-sm text-muted-foreground">
+              {review.category} - Nota {review.rating}/10
+            </p>
+          </div>
+        ))}
+      </section>
     </section>
   );
 }
