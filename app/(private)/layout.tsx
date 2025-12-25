2b150b834916534c73c429db36f1373d6359b1f2
José Isaac <joseisaacnascimento@gmail.com>
2025-12-25 12:03:17 -0300

    feat(layout): adicionar botão de logout no layout privado

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,7 +1,24 @@
+"use client";
+import { useLogout } from "@/lib/useLogout";
+import { Button } from "@/components/ui/button";
+import { LogOut } from "lucide-react";
+
 export default function PrivateLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main>{children}</main>;
+  const { logout } = useLogout();
+  return (
+    <main>
+      <Button
+        onClick={() => {
+          logout();
+        }}
+      >
+        <LogOut className="h-4 w-4" />
+      </Button>
+      {children}
+    </main>
+  );
 }
