7deb7b3a8ce096fe9beba1b5f879eec17afe4317
José Isaac <joseisaacnascimento@gmail.com>
2025-12-25 16:59:36 -0300

    feat(header): criar componente de header - importar em layout publico

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,7 +1,14 @@
+import Header from "@/components/Header";
+
 export default function PublicLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <main>{children}</main>;
+  return (
+    <main>
+      <Header />
+      {children}
+    </main>
+  );
 }
