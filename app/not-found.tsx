9d39dadfc6b2d98982a1c78fdecf3171453785d4
José Isaac <joseisaacnascimento@gmail.com>
2025-12-25 12:01:52 -0300

    fix(not-found): corrigir caractere extra

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function NotFound() {
   return (
     <section>
-      <h1>Ops, página não encontrada!</h1>;
+      <h1>Ops, página não encontrada!</h1>
       <Link href="/">Voltar para a página inicial</Link>
     </section>
   );
