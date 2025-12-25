37f7ce64dab79a532ca799cac993bbe5f936c7dc
José Isaac <joseisaacnascimento@gmail.com>
2025-12-25 15:19:16 -0300

    feat(nav-links): criar arquivo para links de navegação

---
+++ data/navLinks.ts
@@ -0,0 +1,23 @@
+type NavLink = {
+  name: string;
+  href: string;
+};
+
+const navLinks: NavLink[] = [
+  {
+    name: "Início",
+    href: "/",
+  },
+  {
+    name: "Resenhas",
+    href: "/resenhas",
+  },
+  {
+    name: "Nova Resenha",
+    href: "/resenhas/nova",
+  },
+  {
+    name: "Perfil",
+    href: "/perfil",
+  },
+];
