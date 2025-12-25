a962acfa312b132540962fc3907b2f22318a8bfc
José Isaac <joseisaacnascimento@gmail.com>
2025-12-25 11:28:34 -0300

    delete: deletar páginas de testes

--- app/(private)/teste/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Teste() {
-  return <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur quod possimus doloremque itaque, ipsa tempore obcaecati earum laborum minus, modi harum voluptas consequatur quas eius quam delectus tempora, quo dolores?</h1>;
-}
