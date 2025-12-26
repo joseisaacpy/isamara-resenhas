63937d83cde5ce0275a8f563872482ac85f0df6f
José Isaac <joseisaacnascimento@gmail.com>
2025-12-25 21:41:16 -0300

    feat(card-review): adicionar estrelas para classificação com base na nota

--- components/CardReview.tsx
+++ components/CardReview.tsx
@@ -2,18 +2,31 @@ import type { Review } from "@prisma/client";
 import Link from "next/link";
 import { Card } from "./ui/card";
 import { Button } from "./ui/button";
+import { Star } from "lucide-react";
 
 type CardReviewProps = {
   review: Review;
 };
 
 export default function CardReview({ review }: CardReviewProps) {
+  const MAX_RATING = 5;
   return (
-    <Card className="border p-4 rounded">
-      <h2 className="font-semibold">{review.title}</h2>
-      <p className="text-sm text-muted-foreground">
-        {review.category} - Nota {review.rating}/5
-      </p>
+    <Card className="border p-4 rounded shadow-m simple-hover">
+      <h2 className="text-2xl font-semibold">{review.title}</h2>
+      <p className="text-sm text-muted-foreground">{review.category}</p>
+      <div className="flex items-center gap-1">
+        {Array.from({ length: MAX_RATING }).map((_, index) => (
+          <Star
+            key={index}
+            size={20}
+            className={
+              index < review.rating
+                ? "fill-yellow-400 text-yellow-400"
+                : "text-muted-foreground"
+            }
+          />
+        ))}
+      </div>
       <Link href={`/reviews/${review.id}`}>
         <Button>Ver detalhes</Button>
       </Link>
