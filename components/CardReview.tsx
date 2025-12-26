53914ffacaf81751f949b6020cad289f423528ae
José Isaac <joseisaacnascimento@gmail.com>
2025-12-25 21:24:39 -0300

    feat(card-review): criar botão de ver detalhes da resenha com link dinâmico

--- components/CardReview.tsx
+++ components/CardReview.tsx
@@ -1,5 +1,7 @@
 import type { Review } from "@prisma/client";
+import Link from "next/link";
 import { Card } from "./ui/card";
+import { Button } from "./ui/button";
 
 type CardReviewProps = {
   review: Review;
@@ -7,11 +9,14 @@ type CardReviewProps = {
 
 export default function CardReview({ review }: CardReviewProps) {
   return (
-    <Card key={review.id} className="border p-4 rounded">
+    <Card className="border p-4 rounded">
       <h2 className="font-semibold">{review.title}</h2>
       <p className="text-sm text-muted-foreground">
-        {review.category} - Nota {review.rating}/10
+        {review.category} - Nota {review.rating}/5
       </p>
+      <Link href={`/reviews/${review.id}`}>
+        <Button>Ver detalhes</Button>
+      </Link>
     </Card>
   );
 }
