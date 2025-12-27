615610b6c7ca5691be64d8114e5b3c39c7c18776
José Isaac <joseisaacnascimento@gmail.com>
2025-12-27 14:57:57 -0300

    style(header): remover links e deixar apenas o título

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,21 +1,7 @@
-import Link from "next/link";
 export default function Header() {
   return (
-    <header className="bg-red-700 pt-5 flex flex-col gap-2">
-      <h1 className="title-primary font-bold text-white ml-5">Isamara Logo</h1>
-      <nav className="bg-black text-white p-4 shadow-xl/30">
-        <ul className="flex gap-6 items-center justify-center">
-          <li className="text-xl font-bold">
-            <Link href="/">Geral</Link>
-          </li>
-          <li className="text-xl font-bold">
-            <Link href="/">Início</Link>
-          </li>
-          <li className="text-xl font-bold">
-            <Link href="/">Testes</Link>
-          </li>
-        </ul>
-      </nav>
+    <header className="bg-red-700 p-5 flex flex-col items-center justify-center gap-2 shadow-xl/30">
+      <h1 className="title-primary font-bold text-white">Isamara Logo</h1>
     </header>
   );
 }
