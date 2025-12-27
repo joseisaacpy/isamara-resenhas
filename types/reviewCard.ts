4736508b542b26ed6a20ea62e7759ca427f58426
José Isaac <joseisaacnascimento@gmail.com>
2025-12-27 20:09:37 -0300

    fix(review-card): corrigir tipo - permitir campo de imagem ser null - usar enum em Category

--- types/reviewCard.ts
+++ types/reviewCard.ts
@@ -1,9 +1,11 @@
+import type { Category } from "@prisma/client";
+
 export type ReviewCard = {
   id: string;
   title: string;
   content: string;
-  imageUrl: string;
-  category: string;
+  imageUrl: string | null;
+  category: Category;
   rating: number;
   createdAt: Date;
   updatedAt: Date;
