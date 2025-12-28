b9adb0c49e9f729e0b3ea8f007175ed8263c10f4
José Isaac <joseisaacnascimento@gmail.com>
2025-12-28 12:57:40 -0300

    feat(route): criar rota de DELETE para reviews

--- app/api/reviews/[id]/route.ts
+++ app/api/reviews/[id]/route.ts
@@ -32,3 +32,38 @@ export async function GET(
     });
   }
 }
+
+export async function DELETE(
+  request: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
+  try {
+    const { id } = await context.params;
+
+    const review = await prisma.review.findUnique({
+      where: { id: id },
+    });
+
+    if (!review) {
+      return NextResponse.json({
+        error: "Resenha não encontrada",
+        status: 404,
+      });
+    }
+
+    await prisma.review.delete({
+      where: { id: id },
+    });
+
+    return NextResponse.json({
+      message: "Resenha deletada com sucesso!",
+      status: 200,
+    });
+  } catch (error) {
+    console.log(error);
+    return NextResponse.json({
+      error: "Ocorreu um erro ao deletar a resenha",
+      status: 500,
+    });
+  }
+}
