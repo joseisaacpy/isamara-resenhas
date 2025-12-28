8ebbe3652014e477703b8feaa9c5b6f8f4a40d8c
José Isaac <joseisaacnascimento@gmail.com>
2025-12-28 13:50:13 -0300

    feat(header): transformar header em server componente - usar header para pegar cookies e verificar se o admin está logado - se estiver logado, condiciona os botão de perfil e logout

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,7 +1,32 @@
-export default function Header() {
+import Link from "next/link";
+import { cookies } from "next/headers";
+import { User2 } from "lucide-react";
+import LogoutButton from "./LogoutButton";
+
+export default async function Header() {
+  // instancia os cookies
+  const cookieStore = cookies();
+  // verifica se o usuário está autenticado
+  const session = (await cookieStore).get("session_id")?.value;
+  // transforma em boolean se tiver uma sessão
+  const isAuthenticated = !!session;
   return (
-    <header className="bg-red-700 p-5 flex flex-col items-center justify-center gap-2 shadow-xl/30">
-      <h1 className="title-primary font-bold text-white">Isamara Logo</h1>
+    <header className="bg-red-700 px-6 py-4">
+      <nav className="flex items-center justify-between max-w-6xl mx-auto">
+        {/* title do site com link */}
+        <Link href="/" className="title-primary text-white">
+          📑 Isamara Resenhas
+        </Link>
+
+        {/* condicional para verificar se o usuário está autenticado */}
+        {isAuthenticated ? (
+          <LogoutButton />
+        ) : (
+          <Link href="/login">
+            <User2 className="text-white w-6 h-6" />
+          </Link>
+        )}
+      </nav>
     </header>
   );
 }
