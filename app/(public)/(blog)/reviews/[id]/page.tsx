1c0a57f20b25387c33bb8b82a9b469cf88555894
José Isaac <joseisaacnascimento@gmail.com>
2025-12-28 11:37:27 -0300

    style(blog): criar classe para container do blog

--- app/(public)/(blog)/reviews/[id]/page.tsx
+++ app/(public)/(blog)/reviews/[id]/page.tsx
@@ -31,7 +31,7 @@ export default async function ReviewPage({ params }: ReviewPageProps) {
 
   // retorna a página de review
   return (
-    <section className="max-w-3xl mx-auto p-4 pt-6 space-y-4">
+    <section className="section-container-post">
       <h1 className="text-2xl font-bold">{review.title}</h1>
       {/* imagem da review ou o placeholder */}
       <Image
