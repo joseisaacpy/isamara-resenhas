81aa4842339adb0e27faf66db4db0393236dba84
José Isaac <joseisaacnascimento@gmail.com>
2025-12-27 21:28:50 -0300

    feat(next-config): permitir imagens de qualquer dominio

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: false,
   poweredByHeader: false,
+  images: {
+    remotePatterns: [{ protocol: "https", hostname: "*" }],
+  },
 };
 
 export default nextConfig;
